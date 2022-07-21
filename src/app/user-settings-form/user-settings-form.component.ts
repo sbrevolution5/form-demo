da5a6395012745b69bc2e6b6e2419f1dbf228a5e
sbrevolution5 <Sbrevolution5@aim.com>
2022-07-21 12:05:16 -0400

    inject dataservice

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,3 +1,4 @@
+import { DataService } from './../data/data.service';
 import { UserSettings } from './../data/user-settings';
 import { Component, OnInit } from '@angular/core';
 import { NgForm, NgModel } from '@angular/forms';
@@ -16,12 +17,13 @@ export class UserSettingsFormComponent implements OnInit {
     notes: 'notes go here!'
   }
   userSettings : UserSettings={...this.originalUserSettings};
-  constructor() { }
+  constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
   }
   onSubmit(form: NgForm){
     console.log('in onSubmit: ', form.valid);
+    this.dataService.postUserSettingsForm(this.userSettings);
   }
   onBlur(field: NgModel){
     console.log('in onBlur:', field.valid)
